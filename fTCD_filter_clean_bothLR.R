61bc1f1fc22d3c61d7bbfc6f81d29797798c6300
p1981thompson <paul.thompson@psy.ox.ac.uk>
2019-08-08 15:42:03 +0100

    Adapted script to make order generic argument.

--- fTCD_filter_clean_bothLR.R
+++ fTCD_filter_clean_bothLR.R
@@ -31,7 +31,7 @@ if(get_data==1)
 #########################################################################################
 #########################################################################################
 
-fTCD_glm<-function(path)
+fTCD_glm<-function(path,order)
 {
   
   
@@ -42,7 +42,10 @@ fTCD_glm<-function(path)
   samplingrate <- 25 # Sampling rate after downsampling. Raw data is 100Hz, we take 1 in every 4 samples
   heartratemax <- 125
   
-  glm.data<-data.frame(ID=rep(NA,length(filename1)),Lparam1=rep(NA,length(filename1)),Lparam2=rep(NA,length(filename1)),Lparam3=rep(NA,length(filename1)),Lparam4=rep(NA,length(filename1)),L_HRF=rep(NA,length(filename1)),Rparam1=rep(NA,length(filename1)),Rparam2=rep(NA,length(filename1)),Rparam3=rep(NA,length(filename1)),Rparam4=rep(NA,length(filename1)),R_HRF=rep(NA,length(filename1)))
+  
+  glm.data<-data.frame(matrix(NA,nrow=length(filename1),ncol=(((order+2)*2)+3)))
+  names(glm.data)<-c('ID',paste0('Lparam',(1:(order+2))),'L_HRF',paste0('Rparam',(1:(order+2))),'R_HRF')
+  
   
   for(j in 1:length(filename1))
   {
@@ -176,9 +179,9 @@ fTCD_glm<-function(path)
     gamma = fmri.stimulus(scans = dim(rawdata)[1], onsets = c(1,1+which(diff(rawdata$stim_on)!=0))[seq(2, length(c(1,1+which(diff(rawdata$stim_on)!=0))), by = 2)], durations = 500, TR = 1/25,type="gamma",scale=1)
     
     
-    my_des1<-fmri.design(boxcar)
-    my_des2<-fmri.design(canonical)
-    my_des3<-fmri.design(gamma)
+    my_des1<-fmri.design(boxcar, order = order)
+    my_des2<-fmri.design(canonical, order = order)
+    my_des3<-fmri.design(gamma, order = order)
     
     #Left signal glm
     myfit1L<-glm.fit(x=my_des1,y=rawdata$heartbeatcorrected_L[c(seq(from=0, to=length(rawdata[,1]), by=25))[-1]],family=gaussian())
@@ -208,11 +211,11 @@ fTCD_glm<-function(path)
     
     glm.data[j,1] <- strsplit(basename(myfile),'[.]')[[1]][1]
     
-    glm.data[j,6] <- mychoiceL
-    glm.data[j,11] <- mychoiceR
+    glm.data[j,(((order+2)*1)+2)] <- mychoiceL
+    glm.data[j,(((order+2)*2)+3)] <- mychoiceR
     
-    glm.data[j,2:5] <- switch(mychoiceL,boxcar=myfit1L$coefficients,canonical=myfit2L$coefficients,gamma=myfit3L$coefficients)
-    glm.data[j,7:10] <- switch(mychoiceR,boxcar=myfit1R$coefficients,canonical=myfit2R$coefficients,gamma=myfit3R$coefficients)
+    glm.data[j,2:(((order+2)*1)+1)] <- switch(mychoiceL,boxcar=myfit1L$coefficients,canonical=myfit2L$coefficients,gamma=myfit3L$coefficients)
+    glm.data[j,(((order+2)*1)+3):(((order+2)*2)+2)] <- switch(mychoiceR,boxcar=myfit1R$coefficients,canonical=myfit2R$coefficients,gamma=myfit3R$coefficients)
     
     #Left
     myfit.diagL<-glm.diag(switch(mychoiceL,boxcar=myfit1L,canonical=myfit2L,gamma=myfit3L))
@@ -230,8 +233,8 @@ fTCD_glm<-function(path)
                           x=c(rawdata$sec[c(seq(from=0, to=length(rawdata[,1]), by=25))[-1]],rawdata$sec[c(seq(from=0, to=length(rawdata[,1]), by=25))[-1]]),
                           fitted=c(switch(mychoiceL,boxcar=myfit1L$fitted.values,canonical=myfit2L$fitted.values,gamma=myfit3L$fitted.values),switch(mychoiceR,boxcar=myfit1R$fitted.values,canonical=myfit2R$fitted.values,gamma=myfit3R$fitted.values)),Signal=rep(c("Left","Right"),each=length(rawdata$sec[c(seq(from=0, to=length(rawdata[,1]), by=25))[-1]])))
     
-    ggplot(myplotdat,aes(y=y,x=x))+geom_point()+geom_line(aes(y=fitted),color="blue")+theme_bw() + facet_grid(~Signal) 
-  
+    print(ggplot(myplotdat,aes(y=y,x=x))+geom_point()+geom_line(aes(y=fitted),color="blue")+theme_bw() + facet_grid(~Signal))
+    
     
     
     glm_data<-glm.data
@@ -240,12 +243,27 @@ fTCD_glm<-function(path)
   return(glm_data)    
 }
 
-my_results<-fTCD_glm(path=getwd())
+#-----------------------------------------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------------#
+#Set the order
+order=2
+my_results<-fTCD_glm(path=getwd(),order=order)
+
+my_results_diff<-data.frame(matrix(NA,nrow=length(my_results[,1]),ncol=((order+2)+1)))
+names(my_results_diff)<-c('ID',paste0('Dparam',(1:(order+2))))
+my_results_diff$ID<-my_results$ID
+for(i in 1:(order+2))
+{
+  my_results_diff[,(i+1)] <- my_results[,(i+1)]-my_results[,((order+2)+2+i)]
+}
+#-----------------------------------------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------------#
 
 
+mylong_results_L<-gather(my_results[,1:(order+2+2)],key='param',value='beta',-c(ID,L_HRF))
+mylong_results_R<-gather(my_results[,c(1,(((order+2)*1)+3):(((order+2)*2)+3))],key='param',value='beta',-c(ID,R_HRF))
 
-mylong_results_L<-gather(my_results[,1:6],key='param',value='beta',Lparam1:Lparam4)
-mylong_results_R<-gather(my_results[,c(1,7:11)],key='param',value='beta',Rparam1:Rparam4)
+mylong_results_D<-gather(my_results_diff[,1:(order+2+1)],key='param',value='beta',-ID)
 
 names(mylong_results_L)[2]<-"HRF"
 names(mylong_results_R)[2]<-"HRF"
@@ -253,6 +271,7 @@ names(mylong_results_R)[2]<-"HRF"
 mylong_results_L$Signal<-rep('Left',length(mylong_results_L$param))
 mylong_results_R$Signal<-rep('Right',length(mylong_results_R$param))
 
+#-----------------------------------------------------------------------------------------------------------------------#
 
 mylong_results<-dplyr::bind_rows(mylong_results_L,mylong_results_R)
 
@@ -261,3 +280,46 @@ mylong_results$param<-substring(mylong_results$param, 2)
 ggplot(mylong_results,aes(x=beta,group=Signal))+geom_density(aes(fill=Signal),alpha=0.5)+facet_wrap(~param,scales='free')+theme_bw()
 
 ggplot(mylong_results,aes(x=HRF,group=Signal))+geom_bar(aes(fill=Signal,colour=Signal),alpha=0.5,position="dodge")+theme_bw()
+
+#-----------------------------------------------------------------------------------------------------------------------#
+
+ggplot(mylong_results_D,aes(x=beta))+geom_density(fill='blue',alpha=0.5)+facet_wrap(~param,scales='free')+theme_bw()
+
+#-----------------------------------------------------------------------------------------------------------------------#
+
+
+library(psych)
+library(tidyverse)
+
+#load LI based on old doppler analysis method
+
+old_res<-read.csv("WordGen_results.csv")
+
+old_res<-old_res %>% rename(ID=Filename)
+
+compare_results<-merge(my_results_diff,old_res,by='ID',all.x = T)
+
+compare_results$New_LI <- ifelse(compare_results$Dparam1>0,)
+
+
+fmri_data <- read.csv('/Volumes/PSYHOME/PSYRES/pthompson/DVMB/bruckett_reanalysis/Chapter5_fMRI_data.csv')
+
+# Identify factors
+factor_variables <- c('group_cat', 'group_lat', 'sex', 'hand_self_report', 'hand_QHP_cat', 'hand_EHI_cat', 
+                      'Old_fTCD_wg_cat', 'Old_fTCD_pptt_cat', 'fTCD_wg_cat', 'fTCD_pptt_cat')
+for (i in 1:length(factor_variables))
+{factor_ind <- str_which(colnames(fmri_data), paste0('^',factor_variables[i]))
+fmri_data[,factor_ind] <- as.factor(fmri_data[,factor_ind])}
+
+# Relabel group_cat and sex factors for clarity
+# NB: group_cat 0=typical; 1=atypical
+# sex 0=male; 1=female
+levels(fmri_data$group_cat) <- c('T', 'A')
+levels(fmri_data$sex) <- c('M', 'F')
+
+
+fmri_data<-fmri_data[,c('ID','fMRI_diff_wg_frontal','fMRI_diff_wg_temporal','fMRI_diff_wg_MCA')]
+
+compare_results2<-merge(compare_results,fmri_data,by='ID')
+
+psych::pairs.panels(compare_results2[,c('fMRI_diff_wg_frontal','fMRI_diff_wg_temporal','fMRI_diff_wg_MCA','LI','Dparam1')])
